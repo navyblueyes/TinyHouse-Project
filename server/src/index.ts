fe8898bd3ef93b84a9fad2b66406c074137b29ca
Michael Victor Minkoff <lock_ed_on@yahoo.com>
2021-03-21 22:18:25 -0700

    created delete listing route

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,30 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+import { listings } from "./listings";
+
 const app = express();
 const port = 9000;
+
+app.use(bodyParser.json());
 
 const one: number = 1;
 const two: number = 2;
 
 app.get("/", (req, res) => res.send(` 1 + 2 = ${one + two}`));
+
+app.get("/listings", (req, res) => {
+  return res.send(listings);
+});
+
+app.post("/delete-listing", (req, res) => {
+  const id: string = req.body.id;
+
+  for (let i = 0; i < listings.length; i++) {
+    if (listings[i].id === id) {
+      return res.send(listings.splice(i, 1));
+    }
+  }
+});
 
 app.listen(port);
 
