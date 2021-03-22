36e5e3b89903b970b7d031a817f36e96b9bb2583
Michael Victor Minkoff <lock_ed_on@yahoo.com>
2021-03-22 14:53:46 -0700

    switched from REST API to GraphQL

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,30 +1,12 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-import { listings } from "./listings";
+import { ApolloServer } from "apollo-server-express";
+import { schema } from "./graphql";
 
 const app = express();
 const port = 9000;
 
-app.use(bodyParser.json());
-
-const one: number = 1;
-const two: number = 2;
-
-app.get("/", (req, res) => res.send(` 1 + 2 = ${one + two}`));
-
-app.get("/listings", (req, res) => {
-  return res.send(listings);
-});
-
-app.post("/delete-listing", (req, res) => {
-  const id: string = req.body.id;
-
-  for (let i = 0; i < listings.length; i++) {
-    if (listings[i].id === id) {
-      return res.send(listings.splice(i, 1));
-    }
-  }
-});
+const server = new ApolloServer({ schema });
+server.applyMiddleware({ app, path: "/api" });
 
 app.listen(port);
 
