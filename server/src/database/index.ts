550d36c373c5d3d3cd1172ca5a25bb6f325debbf
Michael Victor Minkoff <lock_ed_on@yahoo.com>
2021-03-23 17:05:52 -0700

    conformed MongoDB into types

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,9 +1,10 @@
+import { Database } from "../lib/types";
+
 const MongoClient = require("mongodb").MongoClient;
 
-// const uri = `mongodb+srv://${user}:${userPassword}@cluster0.csosv.mongodb.net/main?retryWrites=true&w=majority`;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
-export const connectDatabase = async () => {
+export const connectDatabase = async (): Promise<Database> => {
   const client = await MongoClient.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
