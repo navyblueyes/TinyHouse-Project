3ffb037186943b5f303a736a8affbfb011b6afe7
Michael Victor Minkoff <lock_ed_on@yahoo.com>
2021-03-24 00:24:57 -0700

    successfully created delete resolver

--- server/src/graphql/resolvers.ts
+++ server/src/graphql/resolvers.ts
@@ -1,20 +1,35 @@
 import { IResolvers } from "apollo-server-express";
-import { listings } from "../listings";
+import { ObjectId } from "mongodb";
+import { Database, Listing } from "../lib/types";
 
 export const resolvers: IResolvers = {
+  Listing: {
+    id: (listing: Listing) => listing._id.toString(),
+  },
   Query: {
-    listings: () => {
-      return listings;
+    listings: async (
+      _root: undefined,
+      _args: {},
+      { db }: { db: Database }
+    ): Promise<Listing[]> => {
+      return await db.listings.find({}).toArray();
     },
   },
   Mutation: {
-    deleteListing: (_root: undefined, { id }: { id: string }) => {
-      for (let i = 0; i < listings.length; i++) {
-        if (listings[i].id === id) {
-          return listings.splice(i, 1)[0];
-        }
+    deleteListing: async (
+      _root: undefined,
+      { id }: { id: string },
+      { db }: { db: Database }
+    ): Promise<Listing> => {
+      const deleteResults = await db.listings.findOneAndDelete({
+        _id: new ObjectId(id),
+      });
+
+      if (!deleteResults.value) {
+        throw new Error("delete failed");
       }
-      throw new Error("failed to delete listing");
+
+      return deleteResults.value;
     },
   },
 };
